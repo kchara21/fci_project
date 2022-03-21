68ba28c3f7f24b923ee9af97c93272c6a8b9e3ac
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-20 22:32:24 -0500

    update all

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import * as express from "express";
+import * as cors from 'cors';
+import routes from './routes';
+
+const PORT = process.env.PORT || 3000;
+
+createConnection().then(async () => {
+
+    // create express app
+    const app = express();
+
+    //Middelwares
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static('public'))
+
+    //Routes
+    app.use('/',routes);
+
+    // start express server
+    app.listen(PORT,()=> console.log(`Server running on PORT ${PORT}`));
+
+
+}).catch(error => console.log(error));
