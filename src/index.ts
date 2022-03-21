6f5417830c39d4dab04169cd92952112b48c78ef
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-20 23:57:47 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,7 @@ createConnection().then(async () => {
     app.use(express.json());
     app.use(express.static('public'))
 
-    //Manejar posibles rutas
-    
+ 
 
     //Routes
     app.use('/',routes);
