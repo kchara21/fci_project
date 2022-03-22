4ec8e93d8c4f3fc944443216837965f1a4d87e73
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-21 22:49:22 -0500

    udpate all

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,26 @@
-
 import "reflect-metadata";
+import {createConnection} from "typeorm";
 import * as express from "express";
 import * as cors from 'cors';
 import routes from './routes';
-import helmet from "helmet";
+
+const PORT = process.env.PORT || 3000;
+
+createConnection().then(async () => {
+
+    // create express app
+    const app = express();
+
+    //Middelwares
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.static('public'));
+
+    //Routes
+    app.use('/',routes);
+
+    // start express server
+    app.listen(PORT,()=> console.log(`Server running on PORT ${PORT}`));
 
 
-import  {Application} from 'express';
-
-
-class Server{
-    public app: Application;
-
-    constructor(){
-        this.app = express();
-        this.config();
-        this.routes();
-    }
-
-    config(): void{ //Configurara la propiedad "app"
-        this.app.set('port', process.env.PORT || 3000);
-        //Middlewares
-        this.app.use(cors({credentials: true})); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
-        this.app.use(helmet());
-    
-        
-        this.app.use(express.json()); // Para poder recibir datos JSON y entenderlos.
-        this.app.use(express.urlencoded({extended:false})); // Em caso de querer enviar desde un form. HTML
-        this.app.use(express.static('public'));
-    }
-
-
-    routes(): void{ // Definir de app las rutas de mi servidor
-        this.app.use('/',routes);
-
-    }
-
-    start(): void{ // Para que el servidor empiece a escuchar
-        this.app.listen(this.app.get('port'), ()=>{
-            console.log(`Server on port`, this.app.get('port'));
-        });
-    }
-
-
-}
-
-const server = new Server();
-server.start();
+}).catch(error => console.log(error));
