581f360eca5224cae1e13cf964afc532ee43431d
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-22 00:59:52 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
 import routes from './routes';
-import helmet from 'helmet';
+
 
 import { Application } from 'express';
 
@@ -20,7 +20,7 @@ class Server {
     this.app.set('port', process.env.PORT || 3000);
     //Middlewares
     this.app.use(cors()); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
-    this.app.use(helmet());
+
 
     this.app.use(express.json()); // Para poder recibir datos JSON y entenderlos.
     this.app.use(express.urlencoded({ extended: false })); // Em caso de querer enviar desde un form. HTML
