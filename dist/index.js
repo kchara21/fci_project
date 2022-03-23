0136a2d2560b1104dc3b6f5361ca91c25fcdd20e
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-23 14:43:23 -0500

    asd

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -18,22 +9,24 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
 const helmet_1 = __importDefault(require("helmet"));
 const typeorm_1 = require("typeorm");
-(0, typeorm_1.createConnection)().then((connection) => __awaiter(void 0, void 0, void 0, function* () {
+(0, typeorm_1.createConnection)().then(async (connection) => {
     console.log('DB Connected');
-})).catch(err => console.log(err));
+}).catch(err => console.log(err));
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
         this.config();
         this.routes();
+        console.log('hola');
     }
     config() {
         this.app.set('port', process.env.PORT || 3000);
-        //Middlewares
-        this.app.use((0, cors_1.default)({ credentials: true })); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
-        this.app.use((0, helmet_1.default)());
-        this.app.use(express_1.default.json()); // Para poder recibir datos JSON y entenderlos.
-        this.app.use(express_1.default.urlencoded({ extended: false })); // Em caso de querer enviar desde un form. HTML
+        this.app.use((0, cors_1.default)({ credentials: true }));
+        this.app.use((0, helmet_1.default)({
+            contentSecurityPolicy: false,
+        }));
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: false }));
     }
     routes() {
         this.app.use('/', routes_1.default);
@@ -47,3 +40,4 @@ class Server {
 }
 const server = new Server();
 server.start();
+//# sourceMappingURL=index.js.map
