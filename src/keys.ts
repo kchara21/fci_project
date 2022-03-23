2ed58763b1dd198e7200d08ecdb33ee92024d6b7
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-22 23:33:27 -0500

    Create keys.ts

---
+++ src/keys.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+
+export default{
+    database:{
+        host: 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        username: 'ckl6isr9g01fs40n',
+        password: 'xne9o82k1n9pugve',
+        database: 'wmgpksofxvo3j8hw',
+        logging: true,
+        synchronize: true,
+    }
+
+    
+} 
