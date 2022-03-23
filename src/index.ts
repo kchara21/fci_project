45034e939e3bdce248abb268e22802c4421f8f35
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-22 22:53:52 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,49 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Application } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import helmet from 'helmet';
 import {createConnection} from "typeorm";
+import morgan from 'morgan';
+
+createConnection().then(async connection=>{
+    console.log('DB Connected');
+}).catch(err=>console.log(err));
+
+class Server{
+    public app: Application;
+
+    constructor(){
+        this.app = express();
+        this.config();
+        this.routes();
+    }
+
+    config(): void{ //Configurara la propiedad "app"
+        this.app.set('port', process.env.PORT || 3000);
+        this.app.use(morgan('dev'));
+        //Middlewares
+        this.app.use(cors({credentials: true})); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
+        this.app.use(helmet());
+        
+        this.app.use(express.json()); // Para poder recibir datos JSON y entenderlos.
+        this.app.use(express.urlencoded({extended:false})); // Em caso de querer enviar desde un form. HTML
+    }
 
 
-createConnection().then(async () => {
-  const PORT = process.env.PORT || 3000;
+    routes(): void{ // Definir de app las rutas de mi servidor
+         this.app.use('/',routes);
+         this.app.use(express.static('public'));
+    }
 
-    // create express app
-    const app = express();
-
-    //Middelwares
-    app.use(cors());
-    app.use(helmet())
-    app.use(express.json());
-    app.use(express.static('public'));
-    app.use(express.urlencoded({extended:false})); // Em caso de querer enviar desde un form. HTML
-    //Routes
-    app.use('/',routes);
-    
-
-    // start express server
-    app.listen(PORT,()=> console.log(`Server running on PORT ${PORT}`));
+    start(): void{ // Para que el servidor empiece a escuchar
+        this.app.listen(this.app.get('port'), ()=>{
+            console.log(`Server on port`, this.app.get('port'));
+        });
+    }
 
 
-}).catch(error => console.log(error));
+}
+
+const server = new Server();
+server.start();
