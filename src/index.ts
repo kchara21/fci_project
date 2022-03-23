c70d644c87390e79f82b0bd566f6eee05429cb89
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-23 13:18:15 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -23,18 +23,19 @@ class Server{
        
         //Middlewares
         this.app.use(cors({credentials: true})); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
-        this.app.use(helmet());
+        this.app.use(helmet({contentSecurityPolicy:{
+            directives: {
+                defaultSrc: ["'self' 'unsafe-inline' 'unsafe-eval'"],
+        
+                }
+        }}));
+
+
         
         this.app.use(express.json()); // Para poder recibir datos JSON y entenderlos.
         this.app.use(express.urlencoded({extended:false})); // Em caso de querer enviar desde un form. HTML
 
-        // this.app.use(function (req, res, next) {
-        //     res.setHeader(
-        //       'Content-Security-Policy-Report-Only',
-        //       "default-src 'self' 'unsafe-inline' 'strict-dynamic'; script-src-elem 'self' 'strict-dynamic' https://kit.fontawesome.com/4b9ba14b0f.js https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js; img-src 'self' 'strict-dynamic' https://1.bp.blogspot.com/-gxsOcYWghHA/Xp_izTh4sFI/AAAAAAAAU8s/y637Fwg99qAuzW9na_NT_uApny8Vce95gCEwYBhgL/s1600/header-footer-gradient-bg.png "
-        //     );
-        //     next();
-        //   });
+
     }
 
 
