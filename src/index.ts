229b4fffd9f5513833a4c79f4f6e1a45fa30ef71
kaleb chara <kalebdavidchara@hotmail.com>
2022-03-23 13:31:02 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,54 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import helmet from 'helmet';
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 
-createConnection().then(async connection=>{
+createConnection().then(async connection => {
     console.log('DB Connected');
-}).catch(err=>console.log(err));
+}).catch(err => console.log(err));
 
-class Server{
+class Server {
     public app: Application;
 
-    constructor(){
+    constructor() {
         this.app = express();
         this.config();
         this.routes();
     }
 
-    config(): void{ //Configurara la propiedad "app"
+    config(): void { //Configurara la propiedad "app"
         this.app.set('port', process.env.PORT || 3000);
-       
+
         //Middlewares
-        this.app.use(cors({credentials: true})); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
-        this.app.use(helmet({contentSecurityPolicy:false}));
+        this.app.use(cors({ credentials: true })); // Obtener permiso para acceder a recursos seleccionados desde un servidor, en un origen distinto (dominio) al que pertenece.
+        this.app.use(helmet(
+            {
+                contentSecurityPolicy:
+                {
+                    directives: {
+                        ...helmet.contentSecurityPolicy.getDefaultDirectives()
+                        , "script-src-elem": ["'self'", "'https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js'"]
+                    }
+                }
+            }
+        ));
 
 
-        
+
         this.app.use(express.json()); // Para poder recibir datos JSON y entenderlos.
-        this.app.use(express.urlencoded({extended:false})); // Em caso de querer enviar desde un form. HTML
+        this.app.use(express.urlencoded({ extended: false })); // Em caso de querer enviar desde un form. HTML
 
 
     }
 
 
-    routes(): void{ // Definir de app las rutas de mi servidor
-         this.app.use('/',routes);
-         this.app.use(express.static('public'));
+    routes(): void { // Definir de app las rutas de mi servidor
+        this.app.use('/', routes);
+        this.app.use(express.static('public'));
     }
 
-    start(): void{ // Para que el servidor empiece a escuchar
-        this.app.listen(this.app.get('port'), ()=>{
+    start(): void { // Para que el servidor empiece a escuchar
+        this.app.listen(this.app.get('port'), () => {
             console.log(`Server on port`, this.app.get('port'));
         });
     }
